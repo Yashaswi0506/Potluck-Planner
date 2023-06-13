2d5f78a452eb55275e56a46ebab3016179438d30
doggr <yashaswi@pdx.edu>
2023-06-13 03:01:53 -0400

    Microservice backend done

--- frontend/src/Components/AfterLogin.tsx
+++ frontend/src/Components/AfterLogin.tsx
@@ -1,121 +1,150 @@
-
 //the is a page which will get displayed after user logs in
 //the page will have "Create Potluck" button, along with applicaiton info
 //"My potlucks" heading with two buttons "Potluck I can manage" and "Potluck that I am attending"
 
-
 import { useUserAuth } from "@/Context/AuthContext.tsx";
 //import {AuthenticatedUser, ProfileType} from "@/PotluckTypes.ts";
-import {Route, useNavigate} from "react-router-dom";
-import {useEffect, useState} from "react";
+import { Route, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import {Login} from "@/Components/Login.tsx";
-import {Col, Row} from "react-bootstrap";
+import { Login } from "@/Components/Login.tsx";
+import { Col, Row } from "react-bootstrap";
 
 export const AfterLogin = () => {
-  
   const [events, setEvents] = useState([]);
   const auth = useUserAuth();
-  const[email, setEmail] = useState("");
-  
+  const [email, setEmail] = useState("");
+
   const getUsers = async (email) => {
     const eventsRes = await axios({
-      method: 'search',
+      method: "search",
       url: "http://localhost:8080/events/attended",
-      headers: {"Access-Control-Allow-Origin": "*"},
+      headers: { "Access-Control-Allow-Origin": "*" },
       data: {
-        email: email
-      }
+        email: email,
+      },
     });
     return eventsRes.data;
   };
-  
+
   useEffect(() => {
     if (auth.user && auth.user.uid) {
       setEmail(auth.user.email);
       getUsers(auth.user.email).then(setEvents);
-      
     }
-    
   }, []);
-  
-  
+
   const navigate = useNavigate();
-  
+
   const onCreateOrEditEventButtonClick = (id) => {
-    navigate("/events", {state: {eventID:id}});
+    navigate("/events", { state: { eventID: id } });
   };
-  
+
   const onPotluckButtonclick = (id) => {
     console.log("event id in after login :", id);
-    navigate("/manage_potluck", {state: {eventID:id}});
-    
+    navigate("/manage_potluck", { state: { eventID: id } });
   };
-  
-  
-  
-  
+
   //delete event
   const onDeleteEventButtonclick = (id) => {
-    const  delete_event_req= async () => {
+    const delete_event_req = async () => {
       const result = await axios({
-        method: 'delete',
+        method: "delete",
         url: "http://localhost:8080/events",
-        headers: {"Access-Control-Allow-Origin": "*"},
+        headers: { "Access-Control-Allow-Origin": "*" },
         data: {
-          event_id:id, host_id:email
-        }
-        
+          event_id: id,
+          host_id: email,
+        },
       });
-      
+
       return result.status;
     };
-    
-    delete_event_req().then(value =>{
-      if (value === 200){
+
+    delete_event_req().then((value) => {
+      if (value === 200) {
         console.log("Event Deleted");
         window.location.reload();
-      }
-      else{
+      } else {
         console.log("Event not deleted.");
       }
     });
-    
   };
-  
-  
+
   return (
-    <div>
-      
+    <div className = "mx-5">
+    <div >
       <div>
-        <button className="btn btn-primary btn-circle" onClick={onCreateOrEditEventButtonClick.bind(null, null)}>Create Event</button>
+        <button
+          className="btn  btn-lg btn-wide btn-accent"
+          onClick={onCreateOrEditEventButtonClick.bind(null, null)}
+        >
+          Create Event
+        </button>
       </div>
-      <div>
+      <div className="flex flex-col  items-center justify-center">
         <h2>My Potlucks:</h2>
-        <table border={1}>
+        <div>
+        <table className = "table"  >
           <tbody>
-          <tr>
-            <th>Event Name</th>
-            <th>Event Location</th>
-            <th>Event Date</th>
-          </tr>
-          {events ?
-            events.map((event: { id: number, event_name: string; event_location: string, event_date: string, is_host: string}) => (
-              <tr key={event.id}>
-                <th><button className="btn btn-primary btn-circle" onClick={onPotluckButtonclick.bind(null,event.id)}>{event.event_name}</button></th>
-                <th>{event.event_location}</th>
-                <th>{event.event_date}</th>
-                <th>{event.is_host}</th>
-                <th><button className="btn btn-primary btn-circle" onClick={onCreateOrEditEventButtonClick.bind(null,event.id)}>Edit</button></th>
-                <th><button className="btn btn-primary btn-circle" onClick={onDeleteEventButtonclick.bind(null,event.id)}>Delete</button></th>
-              </tr>
-            ))
-            : "Not found"}
+            <tr>
+              <th>Event Name</th>
+              <th>Event Location</th>
+              <th>Event Date</th>
+            </tr>
+            {events
+              ? events.map(
+                  (event: {
+                    id: number;
+                    event_name: string;
+                    event_location: string;
+                    event_date: string;
+                    is_host: string;
+                  }) => (
+                    <tr key={event.id}>
+                      <th>
+                        <a href="#"
+                           className="text-primary hover:text-primary-light focus:text-primary-light"
+                          onClick={onPotluckButtonclick.bind(null, event.id)}
+                        />
+                          {event.event_name}
+                        
+                      </th>
+                      <th>{event.event_location}</th>
+                      <th>{event.event_date}</th>
+                      <th>{event.is_host}</th>
+                      <th>
+                        <button
+                          className="btn btn-primary btn-circle"
+                          onClick={onCreateOrEditEventButtonClick.bind(
+                            null,
+                            event.id
+                          )}
+                        >
+                          Edit
+                        </button>
+                      </th>
+                      <th>
+                        <button
+                          className="btn btn-primary btn-circle"
+                          onClick={onDeleteEventButtonclick.bind(
+                            null,
+                            event.id
+                          )}
+                        >
+                          Delete
+                        </button>
+                      </th>
+                    </tr>
+                  )
+                )
+              : "Not found"}
           </tbody>
         </table>
+        </div>
       </div>
     </div>
-  
+    </div>
   );
 };
 
---
+++ microservice_maps/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, jsonify ,request
+import requests
+from dotenv import load_dotenv
+import os
+from flask_cors import CORS, cross_origin
+
+load_dotenv()
+
+app = Flask(__name__)
+
+search_url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
+details_url = "https://maps.googleapis.com/maps/api/place/details/json"
+
+key = os.getenv('MAPS_API_KEY')
+
+@app.route('/location' , methods=['POST'])
+def results():
+    location = request.json
+    search_payload = {"key":key, "query":location}
+    search_req = requests.get(search_url, params=search_payload)
+    search_json = search_req.json()
+
+
+    place_id = search_json["results"][0]["place_id"]
+
+    details_payload = {"key":key, "placeid":place_id}
+    details_resp = requests.get(details_url, params=details_payload)
+    details_json = details_resp.json()
+
+    url = details_json["result"]["url"]
+    return jsonify({'result' : url})
+
+
+
+if __name__ ==  "__main__":
+    app.run(host='0.0.0.0', port = 7000 ,debug=True)
