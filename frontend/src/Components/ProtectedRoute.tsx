ae92c879e25d4209e7a0180c0a5277024adaa486
doggr <yashaswi@pdx.edu>
2023-06-13 11:38:52 -0400

    reverting Protected Routes.tsx

--- frontend/src/Components/ProtectedRoute.tsx
+++ frontend/src/Components/ProtectedRoute.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import {Navigate, useNavigate} from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { UserAuthContextProvider, useUserAuth } from "../Context/AuthContext";
 
 export const ProtectedRoute = ({ children }) => {
   const { user } = useUserAuth();
-  const navigate = useNavigate();
   const auth = useUserAuth();
   try {
     
+    
+    
     console.log("Check user in Private: ", user);
-    if (auth.user.authorization != 'authorized' || !user) {
+    if (auth.user.authorization != 'authorized' && !user) {
       console.log(auth.user.authorization);
       return <Navigate to="/" />;
     }
